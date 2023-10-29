29abd78daec9da40b19b90207cd0d58de75cfa31
Adrian-glz01 <alu0101404813@ull.edu.es>
2023-10-29 15:12:32 +0000

    Repositorio listo con el login

---
+++ labRecApp/src/components/login.vue
@@ -0,0 +1,40 @@
+<template class="login_component_template">
+  <div class="login_Component">
+    <img alt="Login logo" src="../assets/login_Logo.png" />
+    <div class="login_form">
+      <input type="text" placeholder="Email" v-model="user_data.email"/> 
+    </div>
+    <div class="login_form">
+      <input type="password" placeholder="Password" v-model="user_data.password"/>
+    </div>
+    <div class="login_form">
+      <button @click="on_click">
+        Login
+      </button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import "../styles/login_style.css"
+
+  export default defineComponent({
+    name: 'LoginComponent',
+    setup() {   
+      const user_data = {
+        email: "",
+        password: ""
+      }  
+      const on_click = () => {
+        console.log(user_data.email)
+        console.log(user_data.password)        
+      }
+      return {
+        user_data,
+        on_click
+      } 
+    }
+  })
+</script>
+<style scoped>
+</style>
