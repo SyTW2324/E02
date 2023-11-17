0e4bdc021076746474a50ed607c66ae55877093f
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2023-11-17 00:05:00 +0000

    Se modifica el programa para adaptarlo al patrón flux

---
+++ labRecApp/frontend/src/interfaces/user_store.ts
@@ -0,0 +1,16 @@
+export interface User {
+  email: string
+  password: string
+}
+
+export interface User_data {
+  name: string;
+  surnames: string;
+  dni: string;
+  phone: number;
+  email: string;
+  password: string;
+  administrator: number;
+  afiliateNumber: number;
+  vacationsDays: number;
+}
