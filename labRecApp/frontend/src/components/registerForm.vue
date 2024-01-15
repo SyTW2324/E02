4c9c5badce9b68ceb9338ecc61a1afe69d4dee2a
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-15 19:01:00 +0000

    Se añade el componente para el formulario de registro laboral

---
+++ labRecApp/frontend/src/components/registerForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="showForm" class="register-form">
+        
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "registerFormComponent",
+    props: {
+        showForm: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup() {
+        // funciones aqui manin
+        return {
+
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
