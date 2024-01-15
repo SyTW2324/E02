c6b8c990d4ac03dd4b35c0019a70c5263abcf9db
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-15 11:41:08 +0000

    Se crea el componente de los datos de perfil del usuario

---
+++ labRecApp/frontend/src/views/userProfile.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <NavBarComponent />
+        <UserProfileComponent />
+        <footerComponent />
+    </div>
+</template>
+
+<script lang="ts">
+import NavBarComponent from "../components/mainNavbar.vue"
+import footerComponent from "../components/footer.vue";
+import UserProfileComponent from "../components/userProfileComponent.vue"
+
+export default {
+    components: {
+        NavBarComponent,
+        UserProfileComponent,
+        footerComponent,
+    },
+};
+</script>
+
+<style scoped>
+.container {
+    position: relative;
+}
+
+.header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+    /* Asegúrate de que el encabezado tenga un índice z mayor que el contenido del usuario */
+}
+
+.user-profile {
+    margin-top: 100px;
+    /* Ajusta según la altura de tu encabezado */
+    text-align: center;
+}
+
+.footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+    /* Asegúrate de que el pie de página tenga un índice z mayor que el contenido del usuario */
+}</style>
