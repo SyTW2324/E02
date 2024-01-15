a43d74455203ea279cdc9d8fa5927031152e959e
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-15 08:41:58 +0000

    Se refactorizan los estilos de la vista del perfil de usuario

--- labRecApp/frontend/src/views/userProfile.vue
+++ labRecApp/frontend/src/views/userProfile.vue
@@ -37,6 +37,7 @@
 </template>
 
 <script lang="ts">
+import "../styles/userProfile_style.css"
 export default {
     data() {
         return {
@@ -55,60 +56,4 @@ export default {
 </script>
 
 <style scoped>
-.user-profile {
-    max-width: 800px;
-    margin: 0 auto;
-}
-
-.profile-header {
-    display: flex;
-    align-items: center;
-}
-
-.profile-photo {
-    border-radius: 50%;
-    width: 100px;
-    height: 100px;
-    margin-right: 20px;
-}
-
-.user-info {
-    h2 {
-        margin: 0;
-    }
-}
-
-.profile-details {
-    margin-top: 20px;
-
-    h3 {
-        margin-bottom: 10px;
-    }
-
-    table {
-        width: 100%;
-
-        tr {
-            td {
-                padding: 8px;
-                border-bottom: 1px solid #ddd;
-            }
-        }
-    }
-}
-
-.extra-info-panel {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 20px;
-
-    .panel-item {
-        flex: 1;
-        margin-right: 20px;
-
-        h3 {
-            margin-bottom: 10px;
-        }
-    }
-}
 </style>
