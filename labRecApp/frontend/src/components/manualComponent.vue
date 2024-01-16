e237d52ef25439e79521e4dda47ca733dc46abf5
Eva <alu0101396385@ull.edu.es>
2024-01-16 21:44:30 +0000

    Creación del componente y de la vista manual, con sus estilos

---
+++ labRecApp/frontend/src/components/manualComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app" id="manual">
+    <div id="contenedor">
+      <embed id="visorPDF" src="manual.pdf" type="application/pdf">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import "../styles/manual_style.css"
+export default {
+  name: "ManualComponent"
+}
+</script>
+<style scoped>
+
+</style>
---
+++ labRecApp/frontend/src/views/manual.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app" id="manual">
+    <header>
+      <NavBarComponent />
+    </header>
+    <ManualComponent />
+    <footer>
+      <footerComponent />
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import NavBarComponent from "../components/mainNavbar.vue"
+import footerComponent from "../components/footer.vue";
+import ManualComponent from "../components/manualComponent.vue"
+
+export default {
+  name: "manualView",
+  components: {
+    NavBarComponent,
+    ManualComponent,
+    footerComponent,
+  }
+};
+</script>
+
+<style scoped>
+</style>
