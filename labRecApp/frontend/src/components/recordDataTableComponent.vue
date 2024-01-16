255343f8b751d2208455114e1e5b1aa391286702
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-16 19:46:59 +0000

    Se añade un nuevo componente para pintar la tabla con los datos

---
+++ labRecApp/frontend/src/components/recordDataTableComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hola</h1>
+</template>
+<script lang="ts">
+
+export default {
+    name: "recordDataTableComponent",
+    setup() {
+
+    }
+}
+</script>
+<style scoped>
+</style>
