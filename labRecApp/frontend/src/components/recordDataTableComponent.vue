c22ca39ca9aa4daac213ceeffd33ee7ed661bd00
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-16 20:03:47 +0000

    Se añade una nueva interfaz para guardar el tipo de respuesta del registro

--- labRecApp/frontend/src/components/recordDataTableComponent.vue
+++ labRecApp/frontend/src/components/recordDataTableComponent.vue
@@ -1,14 +1,55 @@
 <template>
-    <h1>Hola</h1>
-</template>
-<script lang="ts">
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Fecha y Hora</th>
+            <th>Ubicación</th>
+            <th>Acción</th>
+            <th>Estado</th>
+            <th>Jornada</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="record in records" :key="record._id">
+            <td>{{ record.name }}</td>
+            <td>{{ record.email }}</td>
+            <td>{{ record.dateTime }}</td>
+            <td>{{ record.ubication }}</td>
+            <td>{{ record.action }}</td>
+            <td>{{ record.estado }}</td>
+            <td>{{ record.jornada }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
 
-export default {
+  export default {
     name: "recordDataTableComponent",
     setup() {
-
-    }
-}
-</script>
-<style scoped>
-</style>
+      const records = ref([]);
+  
+      onMounted(async () => {
+        try {
+          const response = await axios.get(`https://perfect-cod-pantsuit.cyclic.app/record`);
+          records.value = response.data;
+        } catch (error) {
+          console.error('Error fetching records:', error);
+        }
+      });
+  
+      return { records };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Agrega estilos según sea necesario */
+  </style>
