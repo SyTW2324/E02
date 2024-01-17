ccca76e4b340571312c786db6fef6267d4714889
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-17 22:17:13 +0000

    Se crea un formulario para cambiar la contraseña

---
+++ labRecApp/frontend/src/components/changePassComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="showForm" class="register-form">
+        <div class="form-container">
+            <div class="title">
+              <h2>Registro de Actividad</h2>
+            </div>
+
+            <div class="button-container">
+                <button class="submit-button" @click="submitForm">Enviar</button>
+                <button style="margin-left: 13px;" class="close-button" @click="closeFormWithoutSubmit">Cerrar</button>
+            </div>
+
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+    name: "changePassComponent",
+    props: {
+
+    },
+    components: {
+
+    },
+    setup(props, { emit }) {
+
+        const submitForm = () => {
+
+        }
+
+        const closeFormWithoutSubmit = () => {
+            emit('close')
+        }
+
+        return {
+            submitForm,
+            closeFormWithoutSubmit
+        };
+    },
+};
+</script>
+
+<style scoped>
+</style>
+
--- labRecApp/frontend/src/components/mainNavbar.vue
+++ labRecApp/frontend/src/components/mainNavbar.vue
@@ -8,6 +8,7 @@
       </Menubar>
     </div>
     <RegisterForm v-if="registerRequest" :showForm="registerRequest" @close="closeRegister()" />
+    <changePassComponent v-if="changePassFormRquest" :showForm="changePassFormRquest" @close="closeChangePassForm()"/> 
   </div>
 </template>
 
@@ -18,6 +19,7 @@ import { userAuthentication } from "../tools/store";
 import { useRouter } from 'vue-router';
 import RegisterForm from "../components/registerForm.vue"
 import {ref} from "vue" 
+import changePassComponent from "./changePassComponent.vue"
 
 export default {
   data() {
@@ -25,12 +27,14 @@ export default {
   name: "mainNavbarComponent",
   components: {
     Menubar,
-    RegisterForm
+    RegisterForm,
+    changePassComponent
   },
   setup() {
     const authStore = userAuthentication();
     const router = useRouter();
     var registerRequest = ref(false);
+    var changePassFormRquest = ref(false);
     const logout = () => {
       //console.log("Esta entrando aqui ...")
 
@@ -68,6 +72,14 @@ export default {
       router.push('/calendar')
     }
 
+    const openChangePassForm = () => {
+      changePassFormRquest.value = true;
+    }
+
+    const closeChangePassForm = () => {
+      changePassFormRquest.value = false;
+    }
+
     const items = [
       {
         label: 'Mi cuenta',
@@ -81,7 +93,7 @@ export default {
           {
             label: 'Cambiar la contraseña',
             icon: 'pi pi-fw pi-cog',
-            to: ''
+            command: openChangePassForm
           },
           {
             label: 'Cerrar sesión',
@@ -132,7 +144,9 @@ export default {
       items,
       redirectToMainPage,
       registerRequest,
-      closeRegister
+      closeRegister,
+      closeChangePassForm,
+      changePassFormRquest
     }
   }
 }
