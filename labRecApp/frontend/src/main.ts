9a38fa6b92b5c2bbe20b74146e0ccea3ae6860a2
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-17 10:09:53 +0000

    DATOS DEL USUARIO GUARDADOS

--- labRecApp/frontend/src/main.ts
+++ labRecApp/frontend/src/main.ts
@@ -5,11 +5,18 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import { pinia } from './tools/store';
+import { userAuthentication } from './tools/store'
 
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
 app.use(PrimeVue)
+
+const authStore = userAuthentication();
+// SI EL USUARIO ESTA AUTENTIFICADO SE REFRESCAN SUS DATOS...
+if (authStore.getAuthentication()) {
+    authStore.initialize();
+}
 
 app.mount('#app')
