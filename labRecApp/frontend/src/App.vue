7b0f817410033e1026cdb9301ac2f1155f71e9f0
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-17 10:18:12 +0000

    A ver si va lo del refresh

--- labRecApp/frontend/src/App.vue
+++ labRecApp/frontend/src/App.vue
@@ -8,6 +8,14 @@
 
 <script setup lang="ts">
 import './styles/app_style.css';
+import { onMounted } from 'vue';
+import { userAuthentication } from './tools/store';
+
+const authStore = userAuthentication();
+
+onMounted(() => {
+  authStore.initialize();
+});
 </script>
 
 <style scoped></style>
