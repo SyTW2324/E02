3795976ee46a17488224231a49bd62a3899bb813
Eva <alu0101396385@ull.edu.es>
2024-01-17 17:32:23 +0000

    Creación de la pantalla calendario

---
+++ labRecApp/frontend/src/components/calendarComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="calendario">
+    <Calendar class="componenteCalendario" v-model="date" inline/>
+  </div>
+</template>
+
+<script lang="ts">
+import "../styles/calendar_style.css";
+import Calendar from "primevue/calendar";
+import { ref } from "vue";
+
+export default {
+  name: "CalendarComponent",
+  components: {
+    Calendar
+  },
+  setup() {
+    const date = ref();
+    return {
+      date,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ labRecApp/frontend/src/views/calendar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="app" id="calendar">
+      <header>
+        <NavBarComponent />
+      </header>
+      <CalendarComponent />
+      <footer>
+        <footerComponent />
+      </footer>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import NavBarComponent from "../components/mainNavbar.vue"
+  import footerComponent from "../components/footer.vue";
+  import CalendarComponent from "../components/calendarComponent.vue"
+  
+  export default {
+    name: "calendarView",
+    components: {
+      NavBarComponent,
+      CalendarComponent,
+      footerComponent,
+    }
+  };
+  </script>
+  
+  <style scoped>
+  </style>
