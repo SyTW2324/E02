480272be134f0c196f9b5543352d114062c63743
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-19 12:38:27 +0000

    Cambio del entorno de producción por alcanzar el 80% del nº de peticiones disponibles

--- labRecApp/backend/build/test_funcionts/basicFunction.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testFunction = void 0;
-const testFunction = () => {
-    return "Esto es una prueba basica";
-};
-exports.testFunction = testFunction;
