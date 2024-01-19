173739c60f54385d0c6436471716cc02c6032eb1
DESKTOP-3RE84F9\adria <adrian@intrasoft.es>
2024-01-19 13:16:35 +0000

    ..

--- labRecApp/backend/build/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Creo la URI para conectarme a mongodb
-exports.default = {
-    mongoURI: "mongodb+srv://admin:5VAQCFolJoYxsVqh@lbregistercluster.atjeqit.mongodb.net/?retryWrites=true&w=majority"
-};
--- labRecApp/backend/build/interfaces/token_int.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secretKey = void 0;
-;
-exports.secretKey = 'supersecreto'; //? poner algo mas seguro en un futuro
